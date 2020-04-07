de0d13ee6c88a38fd638c07ab2a664d0a176aa4e
joseph <joseph.beckett@academytrainee.com>
2020-04-07 16:28:23 +0100

    init

---
+++ src/package_name/Test_Classs.java
@@ -0,0 +1,14 @@
+package package_name;
+
+public class Test_Classs {
+	public static void main(String args[]) {
+		String hello = "Hello World";
+		String more = "more text here";
+		int one=1, two=2;
+		System.out.println(hello);
+		System.out.println(more);
+		System.out.println(one + two);
+		
+	}
+
+}
